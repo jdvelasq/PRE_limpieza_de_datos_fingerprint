36a5dd5ea568354e54687e8da16a89c7d6102c13
Alejandro Esteban Muñoz Osorio <almunozo@unal.edu.co>
2024-02-27 22:48:02 -0500

    a

--- clean_data.py
+++ clean_data.py
@@ -7,6 +7,13 @@ import pandas as pd
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
 
+# Lee el archivo de texto utilizando Pandas
+    datos = pd.read_csv(input_file, sep='\t')  # Cambia el separador según sea necesario
+
+# Muestra los primeros registros para verificar si se han leído correctamente
+    return(datos)
+
+
 
 def create_fingerprint(df):
     """Cree una nueva columna en el DataFrame que contenga el fingerprint de la columna 'text'"""
@@ -21,11 +28,38 @@ def create_fingerprint(df):
     # 8. Ordene la lista de tokens y remueve duplicados
     # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
 
+    df["fingerprint"]=df["text"]
+
+    df["fingerprint"]=(
+        df["fingerprint"]
+        .str.strip()
+        .str.lower()
+        .str.replace("-","")
+        .str.replace(",","")
+        .str.split()
+        .apply(lambda x:[nltk.PorterStemmer().stem(w) for w in x])
+        .apply(lambda x: sorted(set(x)))
+        .str.join(" ")
+    )
+
+    return df
+
+
+
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     df = df.copy()
+
+    fingerprints = df.sort_values(by=["fingerprint","text"]).copy()
+    fingerprints = fingerprints.groupby("fingerprint").first().reset_index()
+    fingerprints= fingerprints.set_index("fingerprint")["text"].to_dict()
+    df["cleaned"]=df["fingerprint"].map(fingerprints)
+
+    return df
+
+
 
     # 1. Ordene el dataframe por 'fingerprint' y 'text'
     # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
@@ -37,6 +71,11 @@ def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
     # Solo contiene una columna llamada 'texto' al igual
     # que en el archivo original pero con los datos limpios
+    df=df.copy()
+    df=df[("cleaned")]
+    df = pd.DataFrame(df)
+    df=df.rename(columns={"cleaned": "text"})
+    df.to_csv(output_file, sep="\t", index=False)
 
 
 def main(input_file, output_file):
